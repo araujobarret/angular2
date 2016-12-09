67ad9d1b79857db59eef8d97970d5cc349d9679a
Carlos <tiamatpontal@gmail.com>
2016-12-09 12:26:09 -0200

    Udemy lesson 24, custom event binding

--- udemy/project1/src/app/databinding/databiding.component.ts
+++ udemy/project1/src/app/databinding/databiding.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class DatabidingComponent {
   stringInterpolation = 'This is a string interpolation.';
   numberInterpolation = 2;
+
+  onClicked(value: string){
+    alert(value);
+  }
 }
