66d74cb091d7c12d5e7c6c62b6aca0a404035d00
Carlos <tiamatpontal@gmail.com>
2016-12-09 12:13:13 -0200

    Udemy lesson 23, event binding

--- udemy/project1/src/app/app.module.ts
+++ udemy/project1/src/app/app.module.ts
@@ -4,14 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { DatabidingComponent } from './databiding/databiding.component';
-import { PropertyBindingComponent } from './databiding/property-binding.component';
+import { DatabidingComponent } from './databinding/databiding.component';
+import { PropertyBindingComponent } from './databinding/property-binding.component';
+import { EventBindingComponent } from './databinding/event-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DatabidingComponent,
     PropertyBindingComponent,
+    EventBindingComponent,
   ],
   imports: [
     BrowserModule,
