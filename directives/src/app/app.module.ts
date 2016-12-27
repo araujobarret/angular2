3a09336d5986c480b64b1264a960cd57ed506703
Carlos <tiamatpontal@gmail.com>
2016-12-27 15:46:58 -0200

    Directives - ngIf - manipulating DOM

---
+++ directives/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { DirectiveNgifComponent } from './directive-ngif/directive-ngif.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DirectiveNgifComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
