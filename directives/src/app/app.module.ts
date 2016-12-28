692e964c64130dbd0436f8c528e56da844324fdd
Carlos <tiamatpontal@gmail.com>
2016-12-28 20:33:17 -0200

    Directives - ngFor - courses list

--- directives/src/app/app.module.ts
+++ directives/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { Ng2BootstrapModule } from 'ng2-bootstrap/ng2-bootstrap'
 import { AppComponent } from './app.component';
 import { DirectiveNgifComponent } from './directive-ngif/directive-ngif.component';
 import { DirectiveNgswitchComponent } from './directive-ngswitch/directive-ngswitch.component';
+import { DirectiveNgforComponent } from './directive-ngfor/directive-ngfor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DirectiveNgifComponent,
-    DirectiveNgswitchComponent
+    DirectiveNgswitchComponent,
+    DirectiveNgforComponent
   ],
   imports: [
     BrowserModule,
