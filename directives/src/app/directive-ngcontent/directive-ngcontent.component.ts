6b99a7523aeed68d5370f08b6bfb4121ad00eaf0
Carlos <tiamatpontal@gmail.com>
2016-12-29 12:11:01 -0200

    Directives - ngContent - one argument passed

---
+++ directives/src/app/directive-ngcontent/directive-ngcontent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directive-ngcontent',
+  templateUrl: './directive-ngcontent.component.html',
+  styleUrls: ['./directive-ngcontent.component.css']
+})
+export class DirectiveNgcontentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
