25a83ff0d85bc5f870553d640a3c9241a57da8e1
Carlos <tiamatpontal@gmail.com>
2017-01-01 16:30:15 -0200

    Directives - HostListener with HostBinding

--- directives/src/app/shared/highlight.directive.ts
+++ directives/src/app/shared/highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostListener, ElementRef, Renderer } from '@angular/core';
+import { Directive, HostListener, HostBinding, ElementRef, Renderer } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
@@ -6,12 +6,16 @@ import { Directive, HostListener, ElementRef, Renderer } from '@angular/core';
 export class HighlightDirective {
 
   @HostListener('mouseenter') onMouseOver(){
-    this.renderer.setElementStyle(this.element.nativeElement, 'background-color', 'yellow');
+    //this.renderer.setElementStyle(this.element.nativeElement, 'background-color', 'yellow');
+    this.backgroundColor = 'yellow';
   }
 
   @HostListener('mouseleave') onMouseLeave(){
-    this.renderer.setElementStyle(this.element.nativeElement, 'background-color', 'white');
+    //this.renderer.setElementStyle(this.element.nativeElement, 'background-color', 'white');
+    this.backgroundColor = 'white';
   }
+
+  @HostBinding('style.backgroundColor') backgroundColor: string;
 
   constructor(private element: ElementRef, private renderer: Renderer) {
 
