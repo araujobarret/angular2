8603869d49a18d7254262bcc9ee1c9f44283c604
Carlos Alberto de Araujo Barreto <tiamatpontal@gmail.com>
2017-01-25 16:55:04 -0200

    RecipeBook - ShoppingListService implemented with ingredients

--- udemy/recipe-book/src/app/recipe.service.ts
+++ udemy/recipe-book/src/app/recipe.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Recipe } from './recipes/recipe'
+import {Ingredients} from "./shared/ingredients";
 
 @Injectable()
 export class RecipeService {
 
   private recipes: Recipe[] = [
-    new Recipe('Hue', 'Hue é o brasilleiro fazendo brasileirice', 'http://t12.deviantart.net/aEJDWcPQhc0m7CBGHbzArVB2970=/300x200/filters:fixed_height(100,100):origin()/pre05/46de/th/pre/f/2016/229/d/8/rockman_zero_pixel_art_by_maxewenx-daea0d2.png', []),
-    new Recipe('Huehue', 'Ha ha ha ha', 'http://orig14.deviantart.net/7d70/f/2013/097/0/1/project_rcl__rockman_ciel__rebirth__by_genericmav-d60rjk5.png', [])
+    new Recipe('Hue', 'Hue é o brasilleiro fazendo brasileirice', 'http://t12.deviantart.net/aEJDWcPQhc0m7CBGHbzArVB2970=/300x200/filters:fixed_height(100,100):origin()/pre05/46de/th/pre/f/2016/229/d/8/rockman_zero_pixel_art_by_maxewenx-daea0d2.png',
+      [
+        new Ingredients('Booster accell', 2),
+        new Ingredients('Plasma Sword', 1)
+      ]),
+    new Recipe('Huehue', 'Ha ha ha ha', 'http://orig14.deviantart.net/7d70/f/2013/097/0/1/project_rcl__rockman_ciel__rebirth__by_genericmav-d60rjk5.png',
+      [
+        new Ingredients('Blinded dress', 1),
+        new Ingredients('Wheel for jump boost', 4)
+      ])
   ];
 
   getRecipes(){
