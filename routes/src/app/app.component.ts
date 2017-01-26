80a4e5f3da76a09eca348a25ac6f71851ea37a9e
Carlos Alberto de Araujo Barreto <tiamatpontal@gmail.com>
2017-01-26 13:38:18 -0200

    Routes - styling

--- routes/src/app/app.component.ts
+++ routes/src/app/app.component.ts
@@ -2,7 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styles: [
+    `
+      .active {
+        color: red;
+      }
+    `
+  ]
 })
 export class AppComponent {
 
