82ba330e6e9e700af66d63254a7e73ce93d54248
Carlos Alberto de Araujo Barreto <tiamatpontal@gmail.com>
2017-01-27 17:21:53 -0200

    Forms - data driven - radio button and email validation

--- forms/src/app/data-driven/data-driven.component.ts
+++ forms/src/app/data-driven/data-driven.component.ts
@@ -10,11 +10,16 @@ export class DataDrivenComponent implements OnInit {
   // (<FormArray>form.controls['']).push(new FormControl('', asdsadas, asdsa);
   form: FormGroup;
 
+  genders = ['male' , 'female'];
+
   constructor() {
     this.form = new FormGroup({
-      'username': new FormControl('Max', Validators.required),
-      'email': new FormControl('', [Validators.required, Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")]),
-      'password': new FormControl('', Validators.required)
+      'userData': new FormGroup({
+        'username': new FormControl('Max', Validators.required),
+        'email': new FormControl('', [Validators.required, Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")])
+      }),
+      'password': new FormControl('', Validators.required),
+      'gender': new FormControl('male')
     });
   }
 
