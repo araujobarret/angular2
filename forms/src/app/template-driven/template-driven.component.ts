8eea8b987ca0fe77545bd4e77ca31d6352d0fbf0
Carlos Alberto de Araujo Barreto <tiamatpontal@gmail.com>
2017-01-27 09:52:10 -0200

    Forms - template driven - stylying controls

--- forms/src/app/template-driven/template-driven.component.ts
+++ forms/src/app/template-driven/template-driven.component.ts
@@ -3,7 +3,11 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-template-driven',
   templateUrl: './template-driven.component.html',
-  styles: []
+  styles: [`
+    .ng-invalid {
+      border-color: red;
+    }
+  `]
 })
 export class TemplateDrivenComponent {
   onSubmit(form: NgForm){
