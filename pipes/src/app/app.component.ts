c4c899c5950e3f7b79e736528d3ec1f19e83c82d
Carlos Alberto de Araujo Barreto <tiamatpontal@gmail.com>
2017-01-30 11:27:11 -0200

    Pipes - built-in pipes

---
+++ pipes/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styles: [`
+    .pipes {
+      margin: 32px;
+      padding: 32px;
+    }
+  `]
+})
+export class AppComponent {
+  myValue: string = "lowercase";
+  myDate = new Date(2017, 1, 30);
+}
