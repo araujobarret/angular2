0b506685956a33273eaf16003dbe1d8dbcabb338
Carlos <tiamatpontal@gmail.com>
2017-02-05 00:23:36 -0200

    Menu interface implemented

--- users/src/app/login/login.component.ts
+++ users/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from "../shared/auth.service";
 import {FormGroup, FormBuilder, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -11,7 +12,7 @@ export class LoginComponent implements OnInit {
   isSignUp: boolean = true;
   formLogin: FormGroup;
 
-  constructor(private fb: FormBuilder, private auth: AuthService) { }
+  constructor(private fb: FormBuilder, private auth: AuthService, private router: Router) { }
 
   ngOnInit() {
     this.formLogin = this.fb.group({
@@ -24,7 +25,7 @@ export class LoginComponent implements OnInit {
     // Se não for cadastro novo efetua o SignIn
     if(!this.isSignUp){
       this.auth.signIn(this.formLogin.value);
-      console.log("It works!");
+      this.router.navigate(['/menu']);
     }
     else
       this.auth.signUp(this.formLogin.value);
