ed2e7396cb4bdac049206c0b6f39d6fa13b08f56
Carlos <tiamatpontal@gmail.com>
2017-03-16 23:21:16 -0300

    Project lastfm created

---
+++ lastfm/src/app/navigation/navigation.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation',
+  styles: [],
+  template: `
+    <h1>It works!</h1>
+  `
+})
+export class NavigationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
